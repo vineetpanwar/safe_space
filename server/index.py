e9d01d615dfe8d9e2473114b2b835d16977ad278
Vineet Panwar <vineet@nextlevelflag.com>
2024-04-08 08:06:30 -0400

    feat: setup backend for APIs and services

--- server/index.py
+++ server/index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def get_date():
     return jsonify({ 'abc': 'def'})
 
+@app.route('/')
+def home():
+    return jsonify({ 'fooo': 'bar'})
+
 if __name__ == '__main__':
     app.run()
