6e1f6b6db40b7ee9d7f7625e1a2cfb80d30ec423
Vineet Panwar <vineet@nextlevelflag.com>
2024-04-08 08:46:15 -0400

    feat: setup backend for APIs and services

--- server/api/index.py
+++ server/api/index.py
@@ -1,11 +1,14 @@
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def get_date():
-    return jsonify({ 'abc': 'def'})
 
-@app.route('/')
+@app.route("/")
 def home():
-    return jsonify({ 'fooo': 'bar'})
+    return "Flask Vercel Example - Hello World", 200
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"status": 404, "message": "Not Found"}), 404
