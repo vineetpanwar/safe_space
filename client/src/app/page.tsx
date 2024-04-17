464cfe211621faba37c6b815003a734c227344d7
Vineet Panwar <vineet@nextlevelflag.com>
2024-04-17 01:37:26 -0400

    feat: added basic theme

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import SAFE_SPACE_LOGO from "../resources/safe_space_logo.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Safe space chat bot coming soon.....</p>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <Image src={SAFE_SPACE_LOGO} alt="safe space logo" className="h-[60px] w-[60px]" />
+      <p className="text-5xl">Safe Space</p>
     </main>
   );
 }
